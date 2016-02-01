7bce8a58c08ccd6329b2fbdc33d87a7aa851fbc8
JSiedlecky <jakub.w.siedlecki@gmail.com>
2016-02-01 23:06:34 +0100

    main page only 480px

---
+++ script.js
@@ -0,0 +1,8 @@
+//http://lorempixel.com/image_output/animals-q-c-200-141-10.jpg
+//http://lorempixel.com/image_output/animals-q-c-208-167-8.jpg
+
+$(function(){
+	$('li.menuBTN').click(function(){
+		$('li.menuItem').toggle('display');
+	});
+});
